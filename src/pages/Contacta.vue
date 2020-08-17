e7b5c08af5fb8e33479aea5ac0e95ba437710634
Nate Dunn <hello@natedunn.net>
2020-08-16 22:39:16 -0500

    Remove old button styles

--- src/pages/Contacta.vue
+++ src/pages/Contacta.vue
@@ -1,11 +1,13 @@
 <template>
   <Layout>
     <header class="bg-orange-50 bg-repeat-x bg-center">
-      <div class="container pt-24 pb-20 md:py-32 lg:pt-32 lg:pb-24 -mt-16">
+      <div
+        class="container pt-20 pb-12 md:pt-24 md:pb-20 lg:pt-32 lg:pb-24 -mt-14 lg:-mt-16"
+      >
         <transition name="fadeUp">
           <h1
             v-if="show"
-            class="text-3xl md:text-4xl lg:text-5xl mt-8 text-cool-gray-700 leading-snug sm:text-center font-serif font-bold"
+            class="text-3xl md:text-4xl lg:text-5xl mt-8 text-cool-gray-700 leading-snug text-center font-serif font-bold"
             style="animation-duration: 0.3s;"
           >
             Contacta
@@ -52,7 +54,7 @@
             ></textarea>
           </div>
 
-          <button class="mt-4" type="submit">Enviar</button>
+          <button class="mt-4 button-primary" type="submit">Enviar</button>
         </form>
       </div>
     </section>
@@ -101,18 +103,6 @@ textarea {
   @apply appearance-none inline-flex text-gray-700 leading-6 border-t-2 border-r-2 border-b-2 border-l-2 border-gray-400 text-base py-2 px-3 rounded;
   &:focus {
     @apply outline-none bg-white border-orange-400 outline-none shadow-outline-orange;
-  }
-}
-button {
-  @apply inline-flex items-center border-2 justify-center rounded-l-md rounded-r-md font-bold leading-6 border-transparent text-gray-900 bg-orange-400 text-base py-2 px-3;
-  &:hover {
-    @apply bg-orange-300;
-  }
-  &:focus {
-    @apply bg-orange-100 shadow-outline outline-none;
-  }
-  &:active {
-    @apply bg-orange-400;
   }
 }
 </style>
