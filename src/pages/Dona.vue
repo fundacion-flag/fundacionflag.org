3bae14aec2462cc03864bcaf2b3d82879d502edb
Nate Dunn <hello@natedunn.net>
2020-08-16 22:39:34 -0500

    Update header classes

--- src/pages/Dona.vue
+++ src/pages/Dona.vue
@@ -1,11 +1,13 @@
 <template>
   <Layout>
     <header class="bg-orange-50 bg-repeat-x bg-center">
-      <div class="container pt-24 pb-20 md:py-32 lg:pt-32 lg:pb-24 -mt-16">
+      <div
+        class="container pt-20 pb-12 md:pt-24 md:pb-20 lg:pt-32 lg:pb-24 -mt-14 lg:-mt-16"
+      >
         <transition name="fadeUp">
           <h1
             v-if="show"
-            class="text-3xl md:text-4xl lg:text-5xl mt-8 text-cool-gray-700 leading-snug sm:text-center font-serif font-bold"
+            class="text-3xl md:text-4xl lg:text-5xl mt-8 text-cool-gray-700 leading-snug text-center font-serif font-bold"
             style="animation-duration: 0.3s;"
           >
             Dona
