98f0affd1ff42e3314687a914d174eef185ce4a8
Nate Dunn <hello@natedunn.net>
2020-08-16 20:47:20 -0500

    Add new page structure

--- src/pages/Dona.vue
+++ src/pages/Dona.vue
@@ -1,17 +1,36 @@
 <template>
   <Layout>
-    <div class="container mx-auto px-6">
-      <div class="prose prose-xl mt-12">
-        <h1>Dona</h1>
-        <p>Coming soon</p>
+    <header class="bg-orange-50 bg-repeat-x bg-center">
+      <div class="container pt-24 pb-20 md:py-32 lg:pt-32 lg:pb-24 -mt-16">
+        <transition name="fadeUp">
+          <h1
+            v-if="show"
+            class="text-3xl md:text-4xl lg:text-5xl mt-8 text-cool-gray-700 leading-snug sm:text-center font-serif font-bold"
+            style="animation-duration: 0.3s;"
+          >
+            Dona
+          </h1>
+        </transition>
       </div>
-    </div>
+    </header>
+    <section>
+      <div class="container">
+        Coming Soon
+      </div>
+    </section>
   </Layout>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      show: false
+    }
+  },
+  mounted() {
+    this.show = true;
+  }
 }
 </script>
 
