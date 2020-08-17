b13c33c8dcf768d96ecce4338c11816119cf93af
Nate Dunn <hello@natedunn.net>
2020-08-16 19:32:28 -0500

    Add test netlify form

--- src/pages/Contacta.vue
+++ src/pages/Contacta.vue
@@ -1,17 +1,85 @@
 <template>
   <Layout>
-    <div class="container mx-auto px-6">
-      <div class="prose prose-xl mt-12">
-        <h1>Contacta</h1>
-        <p>Coming soon</p>
+    <header class="bg-orange-50 bg-repeat-x bg-center">
+      <div class="container pt-24 pb-20 md:py-32 lg:pt-32 lg:pb-24 -mt-16">
+        <transition name="fadeUp">
+          <h1
+            v-if="show"
+            class="text-3xl md:text-4xl lg:text-5xl mt-8 text-cool-gray-700 leading-snug sm:text-center font-serif font-bold"
+            style="animation-duration: 0.3s;"
+          >
+            Convocatoria 2020
+          </h1>
+        </transition>
       </div>
-    </div>
+    </header>
+    <section>
+      <div class="container">
+        <form
+          name="contact"
+          method="post"
+          v-on:submit.prevent="handleSubmit"
+          action="/success/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label> Don’t fill this out: <input name="bot-field" /> </label>
+          </p>
+          <div class="sender-info">
+            <div>
+              <label for="name" class="label">Your name</label>
+              <input type="text" name="name" v-model="formData.name" />
+            </div>
+            <div>
+              <label for="email">Your email</label>
+              <input type="email" name="email" v-model="formData.email" />
+            </div>
+          </div>
+
+          <div class="message-wrapper">
+            <label for="message">Message</label>
+            <textarea name="message" v-model="formData.message"></textarea>
+          </div>
+
+          <button type="submit">Submit form</button>
+        </form>
+      </div>
+    </section>
   </Layout>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      show: false,
+      formData: {},
+    }
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+    },
+    handleSubmit(e) {
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': e.target.getAttribute('name'),
+          ...this.formData,
+        }),
+      })
+      .then(() => this.$router.push('/success'))
+      .catch(error => alert(error))
+    }
+  },
+  mounted() {
+    this.show = true;
+  }
 }
 </script>
 
