ccc0d85337e3f8029c4261571a713194c8028ee6
Nate Dunn <hello@natedunn.net>
2020-08-17 00:00:42 -0500

    Fix links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,17 +7,35 @@
         © {{ new Date().getFullYear() }} Fundación Lorena Alejandra Gallardo
       </div>
       <ul>
-        <li class="inline-flex items-center mr-4 text-lg">
-          <twitter-icon size="1.25x" class="mr-1" />
-          Twitter
+        <li class="inline-block mr-4 text-lg">
+          <a
+            class="inline-flex items-center"
+            href="https://twitter.com/fundacionflag"
+            target="_blank"
+          >
+            <twitter-icon size="1.25x" class="mr-1" />
+            Twitter
+          </a>
         </li>
-        <li class="inline-flex items-center mr-4 text-lg">
-          <facebook-icon size="1.25x" class="mr-1" />
-          Facebook
+        <li class="inline-block mr-4 text-lg">
+          <a
+            class="inline-flex items-center"
+            href="https://www.facebook.com/fundacionflag"
+            target="_blank"
+          >
+            <facebook-icon size="1.25x" class="mr-1" />
+            Facebook
+          </a>
         </li>
-        <li class="inline-flex items-center mr-4 text-lg">
-          <instagram-icon size="1.25x" class="mr-1" />
-          Instagram
+        <li class="inline-block mr-4 text-lg">
+          <a
+            class="inline-flex items-center"
+            href="https://www.instagram.com/fundacionflag/"
+            target="_blan"
+          >
+            <instagram-icon size="1.25x" class="mr-1" />
+            Instagram
+          </a>
         </li>
       </ul>
     </div>
